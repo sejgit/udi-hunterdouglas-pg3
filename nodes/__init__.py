43b9d4949826a6680087106b993e33cdc9a98730
sejgit <sejgit@users.noreply.github.com>
2024-01-09 22:22:30 -0500

    fix

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,8 +1,8 @@
 
 """ Node classes used by the Python template Node Server. """
 
+from .powerview3      import PowerViewGen3
 from .Scene           import Scene
 from .Shade           import Shade
 from .myNode          import myNode
 from .Controller      import Controller
-from .powerview3      import PowerViewGen3
