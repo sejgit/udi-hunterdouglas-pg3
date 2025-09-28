891f464e1ddd0dced85d001ba8ff3ca26ca56542
sejgit <5984155+sejgit@users.noreply.github.com>
2025-09-27 20:46:09 -0400

    test

--- nodes/Scene.py
+++ nodes/Scene.py
@@ -91,9 +91,7 @@ class Scene(udi_interface.Node):
         This method is called after Polyglot has added the node per the
         START event subscription above
         """
-        self.setDriver('ST', 2,report=True, force=True)
-        self.setDriver('GV0', 0,report=True, force=True)
-        self.setDriver('GV0', self.sid,report=True, force=True)
+        self.setDriver('GV0', self.sid,report=True, force=True, text='new')
 
         # wait for controller start ready
         self.controller.ready_event.wait()
